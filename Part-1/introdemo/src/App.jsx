507e194a6e75246418ad8aee86ea0e37dd317643
Informatic PC <informaticpc07@gmail.com>
2025-07-23 19:27:38 +0100

    part1-Fragment removes extra 'div'

--- Part-1/introdemo/src/App.jsx
+++ Part-1/introdemo/src/App.jsx
@@ -2,18 +2,18 @@ const Hello = (props) => {
   console.log(props);
   
   return (
-    <div>
+    <>
       <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+    </>
   )
 }
 const Wave = ({age, name}) => {
   console.log(`THIS IS: ${ name} ${age}`);
   
   return (
-    <div>
+    <>
       <p>This is the sencond COMP with the name: {name}, and years {age} </p>
-    </div>
+    </>
   )
 }
 
@@ -21,14 +21,14 @@ const App = () => {
   const name = 'REACT';
   const age = 10;
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
 
       <Hello name={name} age={age} />
       <Hello age={12} />
 
       <Wave  name='"MyComp" (so original)' age={25} /> 
-    </div>
+    </>
   )
 }
 
