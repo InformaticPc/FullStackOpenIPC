d1d356afbc17d71f7eb3bbf86723f3096751f0bb
Informatic PC <informaticpc07@gmail.com>
2025-07-23 19:42:58 +0100

    part1-React can render arrays

--- Part-1/introdemo/src/App.jsx
+++ Part-1/introdemo/src/App.jsx
@@ -1,14 +1,10 @@
 
 
 const App = () => {
-  const friends = [
-    {name: 'Ruso', age: 5},
-    {name: 'Elliot', age: 8},
-  ]
+  const friends = ['Ruso', 10 ]
   return (
     <>
-      <p>My friend: {friends[0]}</p>
-      <p>My friend: {friends[1].age}</p> {/*<-- primitive value can be render */}
+      <p>My friend: {friends}</p> {/* React may render arrays */}
     </>
   )
 }
