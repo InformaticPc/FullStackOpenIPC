485bff1d865e1ba7e2d728a02c2dae9a87e2e6e4
Informatic PC <informaticpc07@gmail.com>
2025-07-25 22:18:01 +0100

    Part-1 Components working

--- Part-1/course_info_app/src/App.jsx
+++ Part-1/course_info_app/src/App.jsx
@@ -1,27 +1,47 @@
+// All data still resides in the App component, which passes the necessary data to each component using props
+
+/** Header takes care of rendering the name of the course */
+const Header = ({ courseName }) => {
+  console.log(courseName);
+  return <h1>{courseName}</h1>;
+};
+
+/**  Content renders the parts and their number of exercises */
+const Content = ({ part, exercise }) => {
+  return (
+    <p>
+      {part} {exercise}
+    </p>
+  );
+};
+
+/** Total renders the total number of exercises */
+const Total = (total) => {
+  console.log(total);
+  console.log(total.ex3);
+
+  console.log(total.ex1 + total.ex2 + total.ex3);
+  return <p>Number of exercises {total.ex1 + total.ex2 + total.ex3} </p>;
+};
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header courseName={course} />
+      <Content part={part1} exercise={exercises1}></Content>
+      <Content part={part2} exercise={exercises2}></Content>
+      <Content part={part3} exercise={exercises3}></Content>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}></Total>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
