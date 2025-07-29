28fe35757b19e9aa3733a352259551e6ac53424c
Informatic PC <informaticpc07@gmail.com>
2025-07-29 17:50:17 +0100

    Part-1 Step-5 1.5

--- Part-1/course_info_app/src/App.jsx
+++ Part-1/course_info_app/src/App.jsx
@@ -1,12 +1,13 @@
 // All data still resides in the App component, which passes the necessary data to each component using props
 
-/** Header takes care of rendering the name of the course */
+/** 'Header' takes care of rendering the name of the course */
 const Header = ({ course }) => {
-  console.log('HEADER ' + course);
-  return <h1>{course}</h1>;
+  console.log('HEADER ⬇️' + typeof course);
+  console.log(course);
+  return <h1>{course.name}</h1>;
 };
 
-/** Refactor Content Comp */
+/** Refactor of Content Comp */
 const Part = (prop) => {
   return (
     <>
@@ -16,9 +17,9 @@ const Part = (prop) => {
     </>
   );
 };
-/**  Content renders the parts and their number of exercises */
+/**  'Content' renders the parts and their number of exercises */
 const Content = ({ parts }) => {
-  console.log('Content Comp ⬇️');
+  console.log('Content Comp ⬇️' + typeof parts);
   console.log(parts);
   return (
     <>
@@ -29,42 +30,47 @@ const Content = ({ parts }) => {
   );
 };
 
-/** Total renders the total number of exercises */
+/** 'Total' renders the total number of exercises */
 const Total = ({ parts }) => {
-  console.log('TOTAL: ⬇️');
+  console.log('TOTAL: ⬇️' + typeof parts);
   console.log(parts);
   console.log(parts[0]);
-
+  // ⬇️ Below I wanted a different way to render the array inside the object
+  // Therefore I only need to call the object 'course' inside the App component in case that later is needed...
   return (
     <p>
       Number of exercises{' '}
-      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      {parts.parts[0].exercises +
+        parts.parts[1].exercises +
+        parts.parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course} />
     </div>
   );
 };
