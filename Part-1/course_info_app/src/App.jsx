b5edfa43aa40a682121abf0b37091138f8613b28
Informatic PC <informaticpc07@gmail.com>
2025-08-04 18:33:57 +0100

    Understanding how handler event works in react to re-render COMP

--- Part-1/course_info_app/src/App.jsx
+++ Part-1/course_info_app/src/App.jsx
@@ -2,17 +2,29 @@ import { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log('###############');
+  console.log('rendering with counter value', counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const deCreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log('increasing value, before', counter);
+    setCounter(counter + 1);
+  };
+
+  const deCreaseByOne = () => {
+    console.log('decreasing value, before', counter);
+    setCounter(counter - 1);
+  };
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  };
 
   return (
     <div>
       <Display counter={counter} />
       <Button event={increaseByOne} name="PLUS" />
-      <Button event={setToZero} name="ZERO" />
       <Button event={deCreaseByOne} name="LESS" />
+      <Button event={setToZero} name="ZERO" />
     </div>
   );
 };
