753fc57643c919e39165a746375f0bb5aa5decef
Informatic PC <informaticpc07@gmail.com>
2025-08-06 17:11:57 +0100

    Part-1d practice-complex-state 2.0

--- Part-1/course_info_app/src/App.jsx
+++ Part-1/course_info_app/src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
+  console.log(`left: ${clicks.left}`);
+  console.log(`right: ${clicks.right}`);
+  // you  can calculate a single property or assign new value but
+  // useState callbackfunction will call the whole object
+  // so whole obj need to be reassign again
+  console.log(`sum: ${clicks.left + 1}`);
+
+  console.log(`Total clicks done: ${clicks.right + clicks.left}`);
+
+  // Like so:
+  // ---------------------
+  const handlerLeftclick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right,
+    };
+    setClicks(newClicks);
+  };
+  const handlerRightclick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1,
+    };
+    setClicks(newClicks);
+  };
+  // ---------------------
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
-      {right}
+      {clicks.left}
+      <button onClick={handlerLeftclick}>left</button>
+      <button onClick={handlerRightclick}>right</button>
+      {clicks.right}
     </div>
   );
 };
