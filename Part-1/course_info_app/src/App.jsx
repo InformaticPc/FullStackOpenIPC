6a0710ea4310dfdb236a22dcd14590f38bce1789
Informatic PC <informaticpc07@gmail.com>
2025-09-01 20:34:58 +0200

    Part-1d 1.14* ISSUE states doesn't update inmediatly

--- Part-1/course_info_app/src/App.jsx
+++ Part-1/course_info_app/src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 
 const NextAnecdote = ({ onClick }) => {
+  console.log('<NextAnecdote /> running');
   return <button onClick={onClick}>next anecdote</button>;
 };
 
 const Vote = ({ onClick, votes }) => {
+  console.log('<Vote /> running');
+
   return (
     <>
       <p>has {votes} votes</p>
       <button onClick={onClick}>vote</button>
     </>
   );
+};
+
+const BestAnecdote = ({ anecdote, voteStart }) => {
+  console.log('<BestAnecdote /> running');
+  if (voteStart == -1) {
+    return <p>Nothing voted yet</p>;
+  }
+  return <p>{anecdote}</p>;
 };
 
 const App = () => {
@@ -25,28 +36,57 @@ const App = () => {
     'The only way to go fast, is to go well.',
   ];
 
+  // states
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
-  console.log(votes);
+  const [bestIndex, setBestIndex] = useState(-1);
+  console.log('votes:', votes);
+  console.log('best index is:', bestIndex);
 
-  // handler event
+  // -----------------------
+
+  // handler events
   const handleRandom_Button = () => {
     const random = Math.floor(Math.random() * anecdotes.length);
     setSelected(random);
   };
 
   const handleVote_button = () => {
+    // Initialize the bestindex for the 1st time
+    console.log(`----bestIndex before----⬇️:`, bestIndex);
+    if (bestIndex == -1) setBestIndex(0);
+    console.log(`----bestIndex after---⬆️:`, bestIndex);
+    // make a copy of org arr to change the state
+    // with a new array modified
     const copy = [...votes];
     copy[selected] += 1;
     setVotes(copy);
+    highestVote();
+  };
+
+  // get the index of highest voted anecdote
+  const highestVote = () => {
+    console.log('highest handler running');
+
+    for (let i = 0; i < votes.length; i++) {
+      if (votes[bestIndex] <= votes[i]) {
+        setBestIndex(i);
+        console.log('### index inside for loop: ', i);
+      }
+    }
   };
 
   return (
     <>
+      <h1>Anecdote of the day</h1>
+
       <div>{anecdotes[selected]}</div>
 
       <Vote onClick={handleVote_button} votes={votes[selected]} />
       <NextAnecdote onClick={handleRandom_Button} />
+
+      <h1>Anecdote with most votes</h1>
+      <BestAnecdote anecdote={anecdotes[bestIndex]} voteStart={bestIndex} />
     </>
   );
 };
