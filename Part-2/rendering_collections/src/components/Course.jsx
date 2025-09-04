23cee1bad823f0795a7c3dca3f15dca7032198f3
Informatic PC <informaticpc07@gmail.com>
2025-09-04 22:39:41 +0200

    Part-2 Step-7 2.2

--- Part-2/rendering_collections/src/components/Course.jsx
+++ Part-2/rendering_collections/src/components/Course.jsx
@@ -1,27 +1,38 @@
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-const Header = ({ header }) => <h1>{header.name}</h1>;
-
-const Content = ({ course }) => {
+const Part = ({ part }) => {
   return (
-    <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
-    </div>
+    <p>
+      {part.name} {part.exercises}
+    </p>
   );
 };
+const Header = ({ header }) => <h1>{header.name}</h1>;
 
-// const Total = (props) => <p>Number of exercises {props.total}</p> <== not needed yet
+const Content = ({ course }) => (
+  <>
+    {course.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Total = ({ sum }) => {
+  let total = 0;
+  sum.parts.map((part) => {
+    total += part.exercises;
+  });
+  return (
+    <>
+      <strong>Total of exercises {total}</strong>
+    </>
+  );
+};
 
 const Course = ({ course }) => {
   return (
     <>
       <Header header={course} />
       <Content course={course} />
+      <Total sum={course} />
     </>
   );
 };
