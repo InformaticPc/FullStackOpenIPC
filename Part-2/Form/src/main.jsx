605e015a7bc2576ca72f687d4a8bde148f887299
Informatic PC <informaticpc07@gmail.com>
2025-11-26 22:52:07 +0100

    FORM NOTES: Rendering server request

--- Part-2/Form/src/main.jsx
+++ Part-2/Form/src/main.jsx
@@ -9,32 +9,13 @@ promise.then((response) => {
 })*/
 
 axios.get("http://localhost:3001/notes").then((response) => {
+  const data = response.data;
   console.log("response: ", response.data);
 
-  const promise2 = axios.get("http://localhost:3001/foobar");
-  console.log(promise2);
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={data} />
+  );
 });
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-notes.fil;
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-);
 // https://fullstackopen.com/en/part2/getting_data_from_server
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
