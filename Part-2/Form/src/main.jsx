76ac14c1b84e34f16a710dd24757e2f2c130c1e1
Informatic PC <informaticpc07@gmail.com>
2025-11-26 22:40:04 +0100

    FORM NOTES: axios.get()

--- Part-2/Form/src/main.jsx
+++ Part-2/Form/src/main.jsx
@@ -1,5 +1,19 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import axios from "axios";
+
+/*const promise = axios.get("http://localhost:3001/notes");
+console.log(promise);
+promise.then((response) => {
+  console.log("Response: ", response.data);
+})*/
+
+axios.get("http://localhost:3001/notes").then((response) => {
+  console.log("response: ", response.data);
+
+  const promise2 = axios.get("http://localhost:3001/foobar");
+  console.log(promise2);
+});
 
 const notes = [
   {
@@ -22,3 +36,5 @@ notes.fil;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App notes={notes} />
 );
+// https://fullstackopen.com/en/part2/getting_data_from_server
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
