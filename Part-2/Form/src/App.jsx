a6a610b7ed71fce48ab94bf008352d54a65bb22e
Informatic PC <informaticpc07@gmail.com>
2026-01-16 09:44:58 +0100

    FORM NOTES: Part-2d  axios post() & pust() method

--- Part-2/Form/src/App.jsx
+++ Part-2/Form/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import "./global.css";
 
 const App = () => {
   // ---------STATES---------
@@ -20,8 +21,10 @@ const App = () => {
     };
 
     const promise = axios.get("http://localhost:3001/notes");
+    console.log(promise);
+
     promise.then(eventHandler, (err) =>
-      console.log("then(): promise rejected | err=>", err)
+      console.log("then(...,err): promise rejected | err=>", err)
     );
   };
   useEffect(hook, []);
@@ -41,13 +44,39 @@ const App = () => {
       important: Math.random() < 0.5, // <-- true/false random num btw 0 <-> 1
     };
 
-    setNotes(notes.concat(addNewNote)); // <-- here they used 'concat' instead of 'push'. Return a new array without modifiying any previous array
-    setNewNote("");
+    // ---------POST NEW NOTES---------
+    const posting = axios.post("http://localhost:3001/notes", addNewNote);
+    console.log("posting:", posting);
+
+    posting.then((response) => console.log("POST response:", response));
+    // ------GET BACK NOTES(no needed because is react)------
+    // but for practicing purposes
+    posting.finally((final) => {
+      console.log("final posting:", final);
+      axios.get("http://localhost:3001/notes").then((response) => {
+        console.log("GET RESPONSE AFTER POSTING:", response);
+
+        setNotes(notes.concat(response.data)); // <-- here they used 'concat' instead of 'push'. Return a new array without modifiying any previous array
+        setNewNote("");
+      });
+    });
   };
   // ---------HANDLERS---------
   const handleNoteChange = (event) => {
     console.log("event.target.value onChange= ", event.target.value);
     setNewNote(event.target.value);
+  };
+
+  // ---------ID Importance---------
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id === id ? response.data : note)));
+    });
+    // need to post the changes...
   };
 
   return (
@@ -58,7 +87,11 @@ const App = () => {
       </button>
       <ol>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ol>
       <form onSubmit={addNote}>
@@ -75,5 +108,6 @@ const App = () => {
 };
 
 export default App;
-// The development runtime environment
-// https://fullstackopen.com/en/part2/getting_data_from_server
+// Changing the Importance of Notes
+// This is accomplished with the map method: <==
+// https://fullstackopen.com/en/part2/altering_data_in_server
