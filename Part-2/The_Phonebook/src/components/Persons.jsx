37b6a43f6112498b810e14900680a6d3fcfc6711
Informatic PC <informaticpc07@gmail.com>
2026-05-05 20:19:30 +0200

    Part-2e The Phonebook Step-11 2.16

--- Part-2/The_Phonebook/src/components/Persons.jsx
+++ Part-2/The_Phonebook/src/components/Persons.jsx
@@ -1,12 +1,12 @@
 const Persons = ({ persons, handlerDel }) => {
-  return persons.map((person) => (
-    <>
+  return persons.map((person) => {
+    return (
       <p key={person.id}>
         {person.name} {person.number}
         <button onClick={() => handlerDel(person)}>Delete contact</button>
       </p>
-    </>
-  ));
+    );
+  });
 };
 
 export default Persons;
